536c08658e6fa9ed2bf3da655e0405d13f34f676
Harald Glatt <code@hach.re>
2015-05-10 17:18:56 +0200

    1.1.20150510.1
    refactor: made printFibonnaci into getFibonacci to return a number array with results
    added: printFibonnaci function to print out the array
    refactor: renamed Program into Fibonnaci and renamed the functions into versions without an appended Fibonacci
    changed: max value is now part of the function parameters instead of a global
    changed: max no longer refers to the biggest number in the sequence but to the number of total values in the sequence instead

--- source/fibonacci.ts
+++ source/fibonacci.ts
@@ -1,29 +1,38 @@
-class Program {
-	static end = 10000;
+class Fibonacci {
+	/* 	
+			get() - finds numbers in the Fibonacci sequence and stores them to an array	
+				required parameters:
+					- max: defines after how many found numbers we should call it quits
+				optional parameters:
+					- prev1: stores very previous value in the sequence so we can resume in recursion
+					- prev2: stores the 2nd previous value in the sequence
+					- array: stores the values of the sequence that have been found so far
+	*/
+	static get(max: number, prev1 = 0, prev2 = 0, array: number[] = []): number[] {
+		// Exit if max has reached 0
+		if (max === 0) { return array; }
 
-	static printFibonacci(prev1?: number, prev2?: number) {
-		// When we have no previous values, assume 0
-		if (!prev1) { prev1 = 0; prev2 = 0; }
-		
 		// The result is the previous two numbers added up
 		var result = prev1 + prev2;
-		console.log(result);
-
-		// Exit when our result has become larger than 10.000
-		if (result >= this.end) { return; }
+		array.push(result);
 		
 		// Store the previous two results
 		prev2 = prev1;
 		prev1 = result;
 
-		// We are cheating here, to get over the 0 state.		
-		if (result == 0) {
-			prev1 = 1;
-		}
+		// First run, we are cheating here, to get over the 0 state.		
+		if (result === 0) { prev1 = 1; }
 	
-		// Call ourselves again for the next result in the sequence	
-		this.printFibonacci(prev1, prev2);
+		// Call ourselves again to find the next result in the sequence	
+		return this.get(max - 1, prev1, prev2, array);
+	}
+	
+	// Simple array print function
+	static print(val: number[]) {
+		for (var i = 0; i < val.length; i++) {
+			console.log("%s: %s", i, val[i]);
+		}
 	}
 }
 
-Program.printFibonacci();
+Fibonacci.print(Fibonacci.get(20));
